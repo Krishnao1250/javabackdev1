41e2c1f944af926fd73f9d38ba3dd85fb42a3428
Krishnao1250 <120150801+Krishnao1250@users.noreply.github.com>
2023-08-07 14:51:30 -0500

    added text file in the java backend dev1

---
+++ demo/src/main/java/com/javabackenddev1/demo/JavaBackendDev1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.javabackenddev1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendDev1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendDev1DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/javabackenddev1/demo/JavaBackendDev1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabackenddev1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendDev1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
